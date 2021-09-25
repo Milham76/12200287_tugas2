cd70cbeae3a0099aa81cba6b21fe6cfe8b7d22eb
Milham76 <m.ilham080502@gmail.com>
2021-09-25 17:05:35 +0700

    menambahkan tugas 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Muhamad Ilham
+ Saya tinggal di daerah Singaparna
+ Nomer Hp 0812345678
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
